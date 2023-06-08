7623afb66df277d46933d0eb18dfc83dfb2986ed
bessax <andrebessax@gmail.com>
2023-06-08 14:55:34 -0300

    #refcator: incrementando a paginação definindo uma classe abstrata para os parâmetros a serem recebidos na query para paginação.

---
+++ APICatalogo/Pagination/QueryStringParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace APICatalogo.Pagination
+{
+    public abstract class QueryStringParameters
+    {
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        const int maxPageSize = 50;
+        public int PageSize
+        {
+            get => _pageSize; set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
--- APICatalogo/Repository/CategoriaRepository.cs
+++ APICatalogo/Repository/CategoriaRepository.cs
@@ -9,15 +9,14 @@ namespace APICatalogo.Repository
     {
         private readonly AppDbContext _context;
         public CategoriaRepository(AppDbContext context):base(context) { }
-            public IEnumerable<Categoria> GetCategoriasProdutos()
+        public IEnumerable<Categoria> GetCategoriasProdutos()
         {
-            return Get().Include(x=>x.Produtos).ToList();
+           return Get().Include(x=>x.Produtos).ToList();
         }
 
         public PagedList<Categoria> GetCategorias(CategoriasParameters parameters)
-        {
-  
-            return PagedList<Categoria>.ToPagedList(Get().OrderBy(x => x.Nome), parameters.PageNumber, parameters.PageSize);
+        {  
+           return PagedList<Categoria>.ToPagedList(Get().Include(x => x.Produtos).OrderBy(x => x.Nome), parameters.PageNumber, parameters.PageSize);
         }
     }
 }
